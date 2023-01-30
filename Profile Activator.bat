f1ddd48a96ca426d7053e7c38ae94d68914edcbd
wubbs37 <78566637+wubbs37@users.noreply.github.com>
2023-01-30 05:11:53 -0500

    Create Profile Activator.bat

---
+++ Profile Activator.bat
@@ -0,0 +1,2 @@
+xcopy "C:\users\%USERNAME%\Documents\My Games\Skyrim Special Edition GOG\MO2\Profiles\Active\*.txt" "C:\users\%USERNAME%\AppData\Local\Skyrim Special Edition GOG\" /exclude:saves /s /y /i
+xcopy "C:\users\%USERNAME%\Documents\My Games\Skyrim Special Edition GOG\MO2\Profiles\Active\*.ini" "C:\users\%USERNAME%\AppData\Local\Skyrim Special Edition GOG\" /exclude:saves /s /y /i 
